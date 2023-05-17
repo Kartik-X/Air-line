877efe6baeff0dc71ef51b7e3ea4abbaf9d0fafb
kartik589123 <meta619012@gmail.com>
2023-05-17 12:28:47 +0530

    setup bodyparser

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("first commitfsdfsdf");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { PORT } = require("./config/serverConfig");
+
+const SetUpAndStartServer = async () => {
+  const app = express();
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.listen(PORT, () => {
+    console.log(`server ${3000} is running`);
+  });
+};
+SetUpAndStartServer();
