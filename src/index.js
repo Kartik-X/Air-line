9b820ba9a08252710d4e523bac54e55faa9becef
kartik589123 <meta619012@gmail.com>
2023-05-31 21:34:27 +0530

    Added airport seeder

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
 const ApiRoutes = require("./routes/index");
 
+const db = require("./models/index");
+//const { City, Airport } = require("./models/index");
+
 const SetUpAndStartServer = async () => {
   const app = express();
 
@@ -11,8 +14,23 @@ const SetUpAndStartServer = async () => {
   app.use(bodyParser.urlencoded({ extended: true }));
 
   app.use("/api", ApiRoutes);
+
   app.listen(PORT, async () => {
     console.log(`server ${PORT} is running`);
+    if (process.env.SYNC_DB) {
+      db.sequelize.sync({ alter: true });
+    }
+
+    // const city = await City.findOne({
+    //   where: {
+    //     id: 17,
+    //   },
+    // });
+    // const airports = await city.getAirports();
+    // // const newAirport = await Airport.create({
+    // //   name: "Jindal International Airport",
+    // //   cityId: 17,
+    // // });
   });
 };
 SetUpAndStartServer();
