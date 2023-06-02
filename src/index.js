47a7a3d6a66b89a32466722ef203e7f36fa2b1f3
kartik589123 <meta619012@gmail.com>
2023-06-02 10:26:53 +0530

    Added airplanes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const { PORT } = require("./config/serverConfig");
 const ApiRoutes = require("./routes/index");
 
 const db = require("./models/index");
-//const { City, Airport } = require("./models/index");
+//const { Airplane } = require("./models/index");
 
 const SetUpAndStartServer = async () => {
   const app = express();
@@ -20,17 +20,6 @@ const SetUpAndStartServer = async () => {
     if (process.env.SYNC_DB) {
       db.sequelize.sync({ alter: true });
     }
-
-    // const city = await City.findOne({
-    //   where: {
-    //     id: 17,
-    //   },
-    // });
-    // const airports = await city.getAirports();
-    // // const newAirport = await Airport.create({
-    // //   name: "Jindal International Airport",
-    // //   cityId: 17,
-    // // });
   });
 };
 SetUpAndStartServer();
